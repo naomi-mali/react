c563d2c4dea1a3cf37e81329da63ccb2a9bd9353
Timea Mali <derzsi_timea@yahoo.com>
2024-08-22 13:02:50 +0000

    props messages

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <FunctionalGreeting />
-      <FunctionalGreetingWithProps />
+      <FunctionalGreetingWithProps greeting="nice to meet you!!!" name="Naomi" age="30"/>
       
     </div>
   );
