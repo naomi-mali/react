f0505a7104cb0f070aedd7c44e0240ee2c895191
Timea Mali <derzsi_timea@yahoo.com>
2024-08-22 16:59:34 +0000

    set state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import FunctionalGreeting from './components/FunctionalGreeting';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+import StatefulGreetingWithCallback from "./components/StatefulGreetingWithCallback";
+import StatefulGreetingWithPrevState from "./components/StatefulGreetingWithPrevState";
 
 function App() {
   return (
     <div className="App">
       <FunctionalGreeting />
       <FunctionalGreetingWithProps greeting="nice to meet you!!!" name="Naomi" age="30"/>
+      <StatefulGreetingWithCallback />
+      <StatefulGreetingWithPrevState />
       
     </div>
   );
