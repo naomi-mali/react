f346218107c278b7eb563c1d39db49369af1dc38
Timea Mali <derzsi_timea@yahoo.com>
2024-08-23 22:42:55 +0000

    Add event handlers

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,12 @@ import FunctionalGreeting from './components/FunctionalGreeting';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
 import StatefulGreetingWithCallback from "./components/StatefulGreetingWithCallback";
 import StatefulGreetingWithPrevState from "./components/StatefulGreetingWithPrevState";
+import EventsClass from "./components/EventsClass";
+import EventsFunctional from "./components/EventsFunctional";
+import EventBinding from "./components/EventBinding";
+import ConditionalRenderingClass from "./components/ConditionalRenderingClass";
+import ConditionalRenderingFunctional from "./components/ConditionalRenderingFunctional";
+import NestingComponents from "./components/NestingComponents";
 
 function App() {
   return (
@@ -12,6 +18,12 @@ function App() {
       <FunctionalGreetingWithProps greeting="nice to meet you!!!" name="Naomi" age="30"/>
       <StatefulGreetingWithCallback />
       <StatefulGreetingWithPrevState />
+      <EventsFunctional />
+      <EventsClass />
+      <EventBinding />
+      <ConditionalRenderingClass />
+      <ConditionalRenderingFunctional />
+      <NestingComponents />
       
     </div>
   );
